65ec233455ea9167b9b2f8987604febff825724e
Gilad Spitzer <giladspitzer@gmail.com>
2020-10-05 14:45:45 +0300

    ch 4 hw

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+    $('#mycarousel').carousel({
+        interval: 3000
+    })
+    $('#carouselButton').click(function(){
+            if($('#carouselButton').children('span').hasClass('fa-pause')){
+                $('#mycarousel').carousel('pause');
+                $('#carouselButton').children('span').removeClass('fa-pause')
+                $('#carouselButton').children('span').addClass('fa-play')
+            }else{
+                $('#mycarousel').carousel('cycle');
+                $('#carouselButton').children('span').removeClass('fa-play')
+                $('#carouselButton').children('span').addClass('fa-pause')
+            }
+            
+
+    })
+})
+$('#reservationToggle').click(function(){
+    $('#reservationModal').modal('toggle');
+})
+
+$('#loginToggle').click(function(){
+    $('#loginModal').modal('toggle');
+})
